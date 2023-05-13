ea93713b6c14827fe0cae60100872bc77565c3a4
신동민/NE <dongmyo@nhnent.com>
2023-05-13 10:20:11 +0900

    * custom filter 데모

--- src/main/java/com/nhnacademy/security/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/security/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.nhnacademy.security.config;
 
+import com.nhnacademy.security.filter.UsernameAdjustingFilter;
+import javax.servlet.Filter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,12 +10,11 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-// TODO #4: security config
 @EnableWebSecurity(debug = true)
 @Configuration
 public class SecurityConfig {
-    // TODO #5: SecurityFilterChain 을 반환하는 Bean 등록
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests()
@@ -33,28 +34,36 @@ public class SecurityConfig {
                 .sessionFixation()
                     .none()
                 .and()
+            // TODO #3: UsernameAdjustingFilter를 UsernamePasswordAuthenticationFilter 앞에 추가
+            .addFilterBefore(usernameAdjustingFilter(), UsernamePasswordAuthenticationFilter.class)
             .build();
     }
 
-    // TODO #6: InMemoryUserDetailsManager 반환하는 Bean 등록
+    // TODO #1: username을 email 형태로 변경
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails admin = User.withUsername("admin")
+        UserDetails admin = User.withUsername("admin@nhnacademy.com")
                 .password("{noop}admin")
                 .authorities("ROLE_ADMIN")
                 .build();
 
-        UserDetails member = User.withUsername("member")
+        UserDetails member = User.withUsername("member@nhnacademy.com")
                 .password("{noop}member")
                 .authorities("ROLE_MEMBER")
                 .build();
 
-        UserDetails guest = User.withUsername("guest")
+        UserDetails guest = User.withUsername("guest@nhnacademy.com")
             .password("{noop}guest")
             .authorities("ROLE_GUEST")
             .build();
 
         return new InMemoryUserDetailsManager(admin, member, guest);
+    }
+
+    // TODO #4: UsernameAdjustingFilter를 빈으로 등록
+    @Bean
+    public UsernameAdjustingFilter usernameAdjustingFilter() {
+        return new UsernameAdjustingFilter("username");
     }
 
 }
---
+++ src/main/java/com/nhnacademy/security/filter/UsernameAdjustingFilter.java
@@ -0,0 +1,51 @@
+package com.nhnacademy.security.filter;
+
+import java.io.IOException;
+import java.util.Objects;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.util.Assert;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+// TODO #2: request parameter로 전달되는 username을 조정하는 필터
+public class UsernameAdjustingFilter extends OncePerRequestFilter {
+    private final String usernameParameter;
+    private final RequestMatcher requestMatcher = new AntPathRequestMatcher("/login", "POST");
+
+
+    public UsernameAdjustingFilter(String usernameParameter) {
+        Assert.notNull(usernameParameter, "usernameParameter cannot be null");
+        this.usernameParameter = usernameParameter;
+    }
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        HttpServletRequest requestToUse = request;
+
+        if (requestMatcher.matches(request)) {
+            requestToUse = new HttpServletRequestWrapper(request) {
+                @Override
+                public String getParameter(String name) {
+                    if (Objects.equals(name, usernameParameter)) {
+                        String username = super.getParameter(name);
+                        if (Objects.nonNull(username) && !username.endsWith("@nhnacademy.com")) {
+                            return username + "@nhnacademy.com";
+                        }
+                    }
+
+                    return super.getParameter(name);
+                }
+            };
+        }
+
+        filterChain.doFilter(requestToUse, response);
+    }
+
+}
